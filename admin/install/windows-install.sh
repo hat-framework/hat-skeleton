b74b479c1aa0f15095843b5f50cf5c03f1eb8fc4
tigredonorte <tigredonorte3@gmail.com>
2014-11-18 08:50:34 -0200

    install.sh inited

---
+++ admin/install/windows-install.sh
@@ -0,0 +1,5 @@
+curl -sS https://getcomposer.org/installer | php -- --install-dir=c:\php
+php c:\\php\\composer.phar self-update
+php c:\\php\\composer.phar update
+cd ..\\..\\Application\\static\\js
+bower update
